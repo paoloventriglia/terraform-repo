4cb26c5386a8c91d97fb91da94ea9b8687ee66ae
Paolo Ventriglia <paolo@corebox.co.uk>
2018-02-15 13:00:58 +0000

    updated .gitignore

--- AWS/VPC/vpc.tf
+++ AWS/VPC/vpc.tf
@@ -1,4 +1,63 @@
-resource "aws_instance" "example" {
-  ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+
+  tags {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.1.0/24"
+
+  tags {
+    Name = "public"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.2.0/24"
+
+  tags {
+    Name = "private"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags {
+    Name = "gw"
+  }
+}
+
+resource "aws_route_table" "pub" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = "${aws_subnet.public.id}"
+  route_table_id = "${aws_route_table.pub.id}"
+}
+
+resource "aws_instance" "pub" {
+  ami           = "ami-d834aba1"
   instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.public.id}"
+}
+
+resource "aws_instance" "priv" {
+  ami           = "ami-d834aba1"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.private.id}"
 }
--- vCloud/vars.tf
+++ vCloud/vars.tf
@@ -2,10 +2,10 @@
 
 # Provider variables
 
-variable "vcd_user" {default = "paolo.ventriglia.local"}
-variable "vcd_pass" {default = "Br19htlit3"}
-variable "vcd_org" {default = "LCG-CORE-DEV"}
-variable "vcd_url" {default = "https://p8v89-vcd.vchs.vmware.com/api/"}
-variable "vcd_vdc" {default = "LCG-DEV-TEST"}
+variable "vcd_user" {default = ""}
+variable "vcd_pass" {default = ""}
+variable "vcd_org" {default = ""}
+variable "vcd_url" {default = "https://xxxxxxx.vchs.vmware.com/api/"}
+variable "vcd_vdc" {default = ""}
 variable "vcd_max_retry_timeout" {default = 60}
 variable "vcd_allow_unverified_ssl" {default = "true"}
